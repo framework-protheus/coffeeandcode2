213406b8a83a1dad57353595eae71592277005c3
afucher <arthurfucher@gmail.com>
2015-09-22 13:17:42 -0300

    route control

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,21 @@
+var OnBeforeActions = {
+    loginRequired: function(pause) {
+      if (!Meteor.userId()) {
+        this.render('home');
+      } else {
+      	this.next();
+      }
+    }
+};
+
+Router.onBeforeAction(OnBeforeActions.loginRequired, {
+	only : ['eventForm']
+});
+
 Router.route('/', function () {
   this.render('home');
+});
+
+Router.route('/eventForm', function () {
+  this.render('eventForm');
 });
