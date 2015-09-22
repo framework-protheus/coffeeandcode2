42d1243c714b900d6f07b95d3c8e90ae36ff0754
felipeconti <felipe@mac.felipeconti.com.br>
2015-09-22 00:36:39 -0300

    Adicionado metodo auxiliar para remover registros

--- server/methods/events.js
+++ server/methods/events.js
@@ -1,11 +1,12 @@
 Meteor.settings.methods.events = {
-    insert : function(){
-        //Make sure the user is logged in before inserting a task
-		/*if (! Meteor.userId()) {
-		 	throw new Meteor.Error("not-authorized");
-		};*/
-		console.log("insert")
-		Events.insert({date:Date()
-		});
-    }
+  insert: function (date) {
+    //Make sure the user is logged in before inserting a task
+    /*if (! Meteor.userId()) {
+     	throw new Meteor.Error("not-authorized");
+    };*/
+    console.log("insert event at", date)
+    Events.insert({
+      date: date || Date()
+    });
+  }
 };
