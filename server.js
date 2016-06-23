d84a165c9349c6895bc3b214eb8bb89c41845f3c
Felipe <felipe.conti@totvs.com.br>
2016-06-23 14:55:18 -0300

    Markdown

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname+"/public"));
-  app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
