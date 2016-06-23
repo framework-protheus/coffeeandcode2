08d56664ef45964a945d119f5d58ef3e1cf69f55
Arthur Fucher <arthurfucher@gmail.com>
2016-06-23 17:59:13 -0300

    added routes and mongoose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/coffeeandcode'); // connect to our database
 
+require('./app/routes')(app);
 app.use('/', express.static(__dirname+"/public"));
-  app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
