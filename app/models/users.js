3092c3ad4f585db31c8ff406462f37d8432693fe
Felipe <felipe.conti@totvs.com.br>
2016-06-29 09:59:17 -0300

    Removing passport-local-mongoose

--- app/models/users.js
+++ app/models/users.js
@@ -2,7 +2,6 @@
 // grab the things we need
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
 const bcrypt = require('bcrypt-nodejs');
 
 // create a schema
@@ -10,8 +9,6 @@ var userSchema = new Schema({
     email: { type: String, required: true }
   , password: { type: String, required: true }
 });
-
-userSchema.plugin(passportLocalMongoose);
 
 userSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
